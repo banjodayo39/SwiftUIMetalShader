69ba462121d01f67c49b19566bc2715aab14458e
Dayo Banjo <dayobanjo231@gmail.com>
2023-07-03 08:35:18 -0700

    Initial Commit

---
+++ SwiftUIMetalShader/SwiftUIMetalShaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMetalShaderApp.swift
+//  SwiftUIMetalShader
+//
+//  Created by Dayo Banjo on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMetalShaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

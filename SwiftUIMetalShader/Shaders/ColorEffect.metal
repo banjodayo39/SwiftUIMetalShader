f0488d5e37a84c5a32955b7f9b6a1478b493bf12
Dayo Banjo <dayobanjo231@gmail.com>
2023-07-04 06:45:50 -0700

    Add pallete shader

--- SwiftUIMetalShader/Shaders/ColorEffect.metal
+++ SwiftUIMetalShader/Shaders/ColorEffect.metal
@@ -13,3 +13,15 @@ using namespace metal;
     bool isColor = (posInChecks.x ^ posInChecks.y) & 1;
     return isColor ? currentColor :  newColor * currentColor.a * sin(time);
 }
+
+float3 pal(float t, float3 a, float3 b, float3 c, float3 d) {
+    return a + b * cos(6.28318 * (c * t + d));
+}
+
+[[ stitchable ]] half4 pallete( float2 position, half4 currentColor, float size, float time)
+{
+    uint2 posInChecks = uint2(position.x / size, position.y / size);
+    bool isColor = posInChecks.y & 1;
+    half3 newColor = 0.5 + 0.5 * cos(time + half3(0, 1, 0.4));
+    return  isColor ? sin(0.25-currentColor*time) : half4(newColor, 1);
+}
--- SwiftUIMetalShader/Views/Color/GradientView.swift
+++ SwiftUIMetalShader/Views/Color/GradientView.swift
@@ -14,18 +14,30 @@ struct GradientViewUI: View {
     let startDate = Date()
     
     var body: some View {
-        TimelineView(.animation) { _ in 
-            ZStack {
-                RoundedRectangle(cornerRadius: 8, style: .continuous)
-                    .fill(LinearGradient(
-                        gradient: .init(colors: [Self.gradientStart, Self.gradientEnd]),
-                        startPoint: .init(x: 0.5, y: 0),
-                        endPoint: .init(x: 0.5, y: 0.6)
-                    ))
-                    .frame(width: 300, height: 200)
-                    .colorEffect(ShaderLibrary.checkerboard(.float(10), .color(.red), .float(startDate.timeIntervalSinceNow)))
+        VStack{
+            TimelineView(.animation) { _ in
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .fill(LinearGradient(
+                            gradient: .init(colors: [Self.gradientStart, Self.gradientEnd]),
+                            startPoint: .init(x: 0.5, y: 0),
+                            endPoint: .init(x: 0.5, y: 0.6)
+                        ))
+                        .frame(width: 300, height: 200)
+                        .colorEffect(ShaderLibrary.checkerboard(.float(10), .color(.red), .float(startDate.timeIntervalSinceNow)))
+                }
+                .padding()
             }
-            .padding()
+            
+            TimelineView(.animation) { _ in
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .fill(Color.blue)
+                        .frame(width: 300, height: 200)
+                        .colorEffect(ShaderLibrary.pallete(.float(10), .float(startDate.timeIntervalSinceNow)))
+                }
+                .padding()
+            }
         }
     }
 }
